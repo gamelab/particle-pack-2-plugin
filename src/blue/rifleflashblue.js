e36437bef4a8e5fb7416313e32590dd589cee6c6
Dave <dave@instinct.co.nz>
2014-05-15 11:04:05 +1200

    Fixed up 2 blue configs

--- src/blue/rifleflashblue.js
+++ src/blue/rifleflashblue.js
@@ -1,17 +1,17 @@
 /**
-  * ParticlePack2 Explosiveflash Effect.
+  * ParticlePack2 Rifleflashblue Effect.
   * @property Bam
   * @type Kiwi.Group
   * @public
   * @static
 */
 
-Kiwi.Plugins.ParticlePack2.Explosiveflashblue = function (state,x,y) { 
-  this.flares1Config = {
-  "numParts": 2,
-  "posOffsetX": 0,
+Kiwi.Plugins.ParticlePack2.Rifleflashblue = function (state,x,y) { 
+  this.bamConfig = {
+  "numParts": 1,
+  "posOffsetX": 30,
   "posOffsetY": 0,
-  "posRadius": 5,
+  "posRadius": 75.6,
   "posRadialStart": 0,
   "posRadialEnd": 6.283185307179586,
   "posWidth": 200,
@@ -20,42 +20,42 @@ Kiwi.Plugins.ParticlePack2.Explosiveflashblue = function (state,x,y) {
   "posAngle": 0,
   "posLength": 200,
   "posRandomLine": true,
-  "posConstrainRadial": false,
+  "posConstrainRadial": true,
   "posRandomRadial": true,
   "posShape": "point",
   "maxVel": 100,
   "minVel": 70,
   "velConstrainRect": true,
-  "velConstrainRadial": true,
-  "velRandomRadial": true,
+  "velConstrainRadial": false,
+  "velRandomRadial": false,
   "velShape": "point",
   "velOffsetX": 0,
   "velOffsetY": 0,
   "velAngMin": 0,
   "velAngMax": 0,
-  "velRadius": 20,
+  "velRadius": 144,
   "velRadialStart": 0,
   "velRadialEnd": 6.283185307179586,
   "velWidth": 200,
   "velHeight": 200,
-  "velAngle": 1.4575697530163152e-7,
-  "velLength": 200,
+  "velAngle": 0,
+  "velLength": 20,
   "velRandomLine": true,
   "minStartTime": 0,
   "maxStartTime": 0,
   "minLifespan": 0.1,
-  "maxLifespan": 0.3,
+  "maxLifespan": 0.2,
   "gravityX": 0,
   "gravityY": 0,
-  "startSize": 150,
-  "endSize": 1,
+  "startSize": 50,
+  "endSize": 20,
   "loop": false,
   "colEnvKeyframes": [
     "0.2",
     "0.3"
   ],
   "alpha": 1,
- "colEnv0": [
+  "colEnv0": [
     0.611764705882353,
     0.9647058823529412,
     0.9921568627450981
@@ -76,28 +76,30 @@ Kiwi.Plugins.ParticlePack2.Explosiveflashblue = function (state,x,y) {
     0.9921568627450981
   ],
   "alphaGradient": [
-    "0",
+    "1",
     "1",
     "1",
     "0"
   ],
   "alphaStops": [
-    "0.1",
+    0.3,
     "0.5"
   ],
   "additive": true,
   "cells": [
-    8
+    0,
+    1,
+    2,
+    3,
+    4
   ],
   "textureID": "_128x128"
 }
-
-
-  this.flares2Config = {
-  "numParts": 9,
+  this.starsConfig = {
+  "numParts": 1,
   "posOffsetX": 30,
   "posOffsetY": 0,
-  "posRadius": 5,
+  "posRadius": 75.6,
   "posRadialStart": 0,
   "posRadialEnd": 6.283185307179586,
   "posWidth": 200,
@@ -106,42 +108,42 @@ Kiwi.Plugins.ParticlePack2.Explosiveflashblue = function (state,x,y) {
   "posAngle": 0,
   "posLength": 200,
   "posRandomLine": true,
-  "posConstrainRadial": false,
+  "posConstrainRadial": true,
   "posRandomRadial": true,
   "posShape": "point",
   "maxVel": 100,
   "minVel": 70,
   "velConstrainRect": true,
-  "velConstrainRadial": true,
-  "velRandomRadial": true,
+  "velConstrainRadial": false,
+  "velRandomRadial": false,
   "velShape": "point",
-  "velOffsetX": 100,
+  "velOffsetX": 0,
   "velOffsetY": 0,
-  "velAngMin": -2,
-  "velAngMax": 2,
-  "velRadius": 20,
+  "velAngMin": 0,
+  "velAngMax": 0,
+  "velRadius": 144,
   "velRadialStart": 0,
   "velRadialEnd": 6.283185307179586,
   "velWidth": 200,
   "velHeight": 200,
-  "velAngle": 1.4575697530163152e-7,
-  "velLength": 200,
+  "velAngle": 0,
+  "velLength": 20,
   "velRandomLine": true,
   "minStartTime": 0,
   "maxStartTime": 0,
   "minLifespan": 0.1,
-  "maxLifespan": 0.3,
+  "maxLifespan": 0.1,
   "gravityX": 0,
   "gravityY": 0,
-  "startSize": 1,
-  "endSize": 150,
+  "startSize": 60,
+  "endSize": 60,
   "loop": false,
   "colEnvKeyframes": [
     "0.2",
     "0.3"
   ],
-  "alpha": 0.7,
- "colEnv0": [
+  "alpha": 1,
+  "colEnv0": [
     0.611764705882353,
     0.9647058823529412,
     0.9921568627450981
@@ -162,32 +164,32 @@ Kiwi.Plugins.ParticlePack2.Explosiveflashblue = function (state,x,y) {
     0.9921568627450981
   ],
   "alphaGradient": [
-    "0",
+    "1",
     "1",
     "1",
     "0"
   ],
   "alphaStops": [
-    "0.1",
+    0.3,
     "0.5"
   ],
   "additive": true,
   "cells": [
-    3,
-    4
+    10,
+    11,
+    12
   ],
   "textureID": "_128x128"
 }
 
   
- var group = new Kiwi.Group(state)
+  var group = new Kiwi.Group(state)
   group.x = x;
   group.y = y;
-  var flares1 = new Kiwi.GameObjects.StatelessParticles(state,state.textures.particlePack2SpriteSheet,0,0,this.flares1Config);
-  var flares2 = new Kiwi.GameObjects.StatelessParticles(state,state.textures.particlePack2SpriteSheet,0,0,this.flares2Config);
-  group.addChild(flares1);
-  group.addChild(flares2);
+  var bam = new Kiwi.GameObjects.StatelessParticles(state,state.textures.particlePack2SpriteSheet,0,0,this.bamConfig);
+  var stars = new Kiwi.GameObjects.StatelessParticles(state,state.textures.particlePack2SpriteSheet,0,0,this.starsConfig);
+  group.addChild(bam);
+  group.addChild(stars);
 
   return group;
 }
-
