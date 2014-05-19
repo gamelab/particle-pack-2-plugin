53da3521e4c4d55b6ea493d4564868e2c02def38
Ross Kettle <ross.p.kettle@gmail.com>
2014-05-20 10:57:00 +1200

    fixed concatenation ordering problem

--- particlepack2-1.0.0.js
+++ particlepack2-1.0.0.js
@@ -1,4 +1,67 @@
 /**
+* For a walkthrough on setting up the particle pack plugin check the <a href="https://github.com/gamelab/particle-pack-1-plugin/blob/master/README.md"><README./a> 
+* @module Kiwi
+* @submodule Kiwi.Plugins
+* @namespace Kiwi.Plugins
+* @class ParticlePack1
+* @main
+*/
+Kiwi.Plugins.ParticlePack2 = {
+  
+  /**
+  * The name of this plugin.
+  * @property name
+  * @type String
+  * @public
+  */
+  name:'ParticlePack2',
+
+  /**
+  * The version of this plugin in semver (semantic versioning) format
+  * @property version
+  * @type String
+  * @public
+  */
+  version:'0.1.0',
+
+  /**
+  * The minimum version of Kiwi.js required to run this plugin in semver (semantic versioning) format
+  * @property minimumKiwiVersion
+  * @type String
+  * @public
+  */
+  minimumKiwiVersion:'0.7.0',
+
+  /**
+  * Other plugins required for this plugin to run. 
+  * @property pluginDependencies
+  * @type Array
+  * @public
+  */
+  pluginDependencies: [
+    {
+      name:'ParticlesGL',
+      minimumVersion:'0.9.0'
+    }
+
+  ]
+
+};
+
+
+
+/**
+* Registers this plugin with the Global Kiwi Plugins Manager if it is avaiable.
+* 
+*/
+Kiwi.PluginManager.register(Kiwi.Plugins.ParticlePack2);
+
+
+ 
+
+ 
+
+/**
   * ParticlePack2 Explosiveflash Effect.
   * @property Bam
   * @type Kiwi.Group
@@ -5355,69 +5418,6 @@ Kiwi.Plugins.ParticlePack2.Sniperflashorange = function (state,x,y) {
   group.addChild(flares5);
   return group;
 }
-
-/**
-* For a walkthrough on setting up the particle pack plugin check the <a href="https://github.com/gamelab/particle-pack-1-plugin/blob/master/README.md"><README./a> 
-* @module Kiwi
-* @submodule Kiwi.Plugins
-* @namespace Kiwi.Plugins
-* @class ParticlePack1
-* @main
-*/
-Kiwi.Plugins.ParticlePack2 = {
-  
-  /**
-  * The name of this plugin.
-  * @property name
-  * @type String
-  * @public
-  */
-  name:'ParticlePack2',
-
-  /**
-  * The version of this plugin in semver (semantic versioning) format
-  * @property version
-  * @type String
-  * @public
-  */
-  version:'0.1.0',
-
-  /**
-  * The minimum version of Kiwi.js required to run this plugin in semver (semantic versioning) format
-  * @property minimumKiwiVersion
-  * @type String
-  * @public
-  */
-  minimumKiwiVersion:'0.7.0',
-
-  /**
-  * Other plugins required for this plugin to run. 
-  * @property pluginDependencies
-  * @type Array
-  * @public
-  */
-  pluginDependencies: [
-    {
-      name:'ParticlesGL',
-      minimumVersion:'0.9.0'
-    }
-
-  ]
-
-};
-
-
-
-/**
-* Registers this plugin with the Global Kiwi Plugins Manager if it is avaiable.
-* 
-*/
-Kiwi.PluginManager.register(Kiwi.Plugins.ParticlePack2);
-
-
- 
-
- 
 
 /**
   * ParticlePack2 Explosiveflashpurple Effect.
